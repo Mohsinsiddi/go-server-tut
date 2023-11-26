fb95a7f630844ce65da0eb087be3fb0bc7d399c7
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-11-26 17:42:19 +0530

    added viper env configs in server

--- main.go
+++ main.go
@@ -6,21 +6,19 @@ import (
 
 	"github.com/Mohsinsiddi/simplebank/api"
 	db "github.com/Mohsinsiddi/simplebank/db/sqlc"
+	"github.com/Mohsinsiddi/simplebank/util"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver ="postgres"
-	dbSource="postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress="0.0.0.0:8080"
 )
 
 
 
 func main(){
-
-	conn, err := sql.Open(dbDriver,dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load configurations:",err)
+	}
+	conn, err := sql.Open(config.DBDriver,config.DBSource)
 
 	if err != nil {
 		log.Fatal("Cannot connect to db:",err)
@@ -29,7 +27,7 @@ func main(){
 	store := db.NewStore(conn)
 	server := api.NewServer(*store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 
 	if err!= nil {
 		log.Fatal("Cannot start server:",err)
