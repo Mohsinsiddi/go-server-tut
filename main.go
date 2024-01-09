ca8b3615d32129e930ec480de989e1b857e4f19f
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2024-01-09 14:18:51 +0530

    added dbdocs and grpc setup with proto files

--- main.go
+++ main.go
@@ -3,39 +3,64 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net"
 
 	"github.com/Mohsinsiddi/simplebank/api"
 	db "github.com/Mohsinsiddi/simplebank/db/sqlc"
+	"github.com/Mohsinsiddi/simplebank/gapi"
+	"github.com/Mohsinsiddi/simplebank/pb"
 	"github.com/Mohsinsiddi/simplebank/util"
-
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 
-
-func main(){
+func main() {
 	config, err := util.LoadConfig(".")
 	if err != nil {
-		log.Fatal("Cannot load configurations:",err)
+		log.Fatal("cannot load config:", err)
 	}
-	conn, err := sql.Open(config.DBDriver,config.DBSource)
 
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
-		log.Fatal("Cannot connect to db:",err)
+		log.Fatal("cannot connect to db:", err)
 	}
 
 	store := db.NewStore(conn)
-	
+	runGrpcServer(config, store)
+}
+
+func runGrpcServer(config util.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener")
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server")
+	}
+}
+
+func runGinServer(config util.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
-	if err!= nil {
-		log.Fatal("Cannot create server:",err)
+	if err != nil {
+		log.Fatal("cannot create server:", err)
 	}
 
-	err = server.Start(config.ServerAddress)
-
-	if err!= nil {
-		log.Fatal("Cannot start server:",err)
+	err = server.Start(config.HTTPServerAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
 	}
-
-	
 }
