b401aed0b9a067d28410523a0e33adcca734ec27
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2024-01-13 23:45:48 +0530

    changed from libpq to pgx

--- gapi/rpc_create_user_test.go
+++ gapi/rpc_create_user_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/Mohsinsiddi/simplebank/worker"
 	mockwk "github.com/Mohsinsiddi/simplebank/worker/mock"
 	"github.com/golang/mock/gomock"
-	"github.com/lib/pq"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -152,7 +151,7 @@ func TestCreateUserAPI(t *testing.T) {
 				store.EXPECT().
 					CreateUserTx(gomock.Any(), gomock.Any()).
 					Times(1).
-					Return(db.CreateUserTxResult{}, &pq.Error{Code: "23505"})
+					Return(db.CreateUserTxResult{}, db.ErrUniqueViolation)
 
 				taskDistributor.EXPECT().
 					DistributeTaskSendVerifyEmail(gomock.Any(), gomock.Any(), gomock.Any()).
--- gapi/rpc_login_user.go
+++ gapi/rpc_login_user.go
@@ -2,7 +2,7 @@ package gapi
 
 import (
 	"context"
-	"database/sql"
+	"errors"
 
 	db "github.com/Mohsinsiddi/simplebank/db/sqlc"
 	"github.com/Mohsinsiddi/simplebank/pb"
@@ -21,7 +21,7 @@ func (server *Server) LoginUser(ctx context.Context, req *pb.LoginUserRequest) (
 	}
 	user, err := server.store.GetUser(ctx, req.GetUsername())
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if errors.Is(err,db.ErrRecordNotFound) {
 			return nil, status.Errorf(codes.NotFound, "user not found")
 		}
 		return nil, status.Errorf(codes.Internal, "failed to find user")
